5540a9058c71cc77e51c9b1c74626528407f06ac
Amirouche <amirouchenaitslimane@gmail.com>
2018-08-30 11:50:35 +0200

    ajouter le checkbox form inscription
    ajouter le checkbox terme et conditions

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,11 +11,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 
 class UserType extends AbstractType
@@ -31,6 +33,12 @@ class UserType extends AbstractType
                 'first_options'  => array('label' => 'Password'),
                 'second_options' => array('label' => 'Repeat Password'),
             ))
+            ->add('termsAccepted', CheckboxType::class, array(
+                'label'    => ' Accepter les termes et conditions ',
+                'mapped' => false,
+                'constraints' => new IsTrue(),
+            ))
+
         ->add('save',SubmitType::class)
         ;
     }
